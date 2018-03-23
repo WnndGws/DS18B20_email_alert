250b48256760ed7e572148a26d607bafdae740f0
WG <wynand@gouws.com.au>
2018-03-23 20:14:32 +0800

    Deleted empty test

--- test_read_temperature.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-##
-import read_temperature
-
-import hypothesis
-import pytest
-
-## I have no idea what needs to be tested
--- verify_email_address.py
+++ verify_email_address.py
@@ -6,54 +6,61 @@ import smtplib
 import sys
 import dns.resolver
 
-def verify_email(input_address, from_address):
-    """
-    Checks that an email address is both valid, and exist
-    The from_address is the field that the script uses for the SMTP MAIL FROM command
-    The input_address is the email to verify
+def check_email_regex(input_address):
+    """Checks that email matches the format of an email address
     """
 
     # Simple Regex for syntax checking
     email_regex = '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$'
-
     # Email address to verify
     address_to_verify = str(input_address)
-
     # Syntax check
     match = re.match(email_regex, address_to_verify)
     if match == None:
-        print('Bad')
+        email_regex_valid = False
     else:
-        # Get domain for DNS lookup
-        splitAddress = address_to_verify.split('@')
-        domain = str(splitAddress[1])
-        print('Domain:', domain)
+        email_regex_valid = True
 
-        # MX record lookup
-        records = dns.resolver.query(domain, 'MX')
-        mxRecord = records[0].exchange
-        mxRecord = str(mxRecord)
+    return email_regex_valid
 
+def verify_email_exists(input_address, from_address):
+    """
+    Checks that an email address exists
+    The from_address is the field that the script uses for the SMTP MAIL FROM command
+    The to_address is the email to verify
+    """
 
-        # SMTP lib setup (use debug level for full output)
-        server = smtplib.SMTP()
-        server.set_debuglevel(0)
+    # Email address to verify
+    address_to_test = str(to_address)
 
-        # SMTP Conversation
-        server.connect(mxRecord)
-        server.helo(server.local_hostname) ### server.local_hostname(Get local server hostname)
-        server.mail(from_address)
-        code, message = server.rcpt(str(address_to_verify))
-        server.quit()
+    # Get domain for DNS lookup
+    splitAddress = address_to_test.split('@')
+    domain = str(splitAddress[1])
+    print('Domain:', domain)
 
-        #print(code)
-        #print(message)
+    # MX record lookup
+    records = dns.resolver.query(domain, 'MX')
+    mxRecord = records[0].exchange
+    mxRecord = str(mxRecord)
 
-        # Assume SMTP response 250 is success
-        if code == 250:
-            print('Success')
-        else:
-            print('Bad')
+    # SMTP lib setup (use debug level for full output)
+    server = smtplib.SMTP()
+    server.set_debuglevel(0)
+
+    # SMTP Conversation
+    server.connect(mxRecord)
+    server.helo(server.local_hostname) ### server.local_hostname(Get local server hostname)
+    server.mail(from_address)
+    code, message = server.rcpt(str(address_to_verify))
+    server.quit()
+
+    # Assume SMTP response 250 is success
+    if code == 250:
+        email_exists = True
+    else:
+        email_exists = False
+
+    return email_exists
 
 if __name__ == '__main__':
-    verify_email("test@gmail.com", "test2@gmail.com")
+    pass
