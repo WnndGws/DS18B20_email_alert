e8f38a3f8437ba930e5c981b4308d084deb8b929
WG <wynand@gouws.com.au>
2018-04-19 14:44:11 +0800

    List comprehension

--- read_temperature.py
+++ read_temperature.py
@@ -22,7 +22,7 @@ def read_temp_raw():
 def parse_temp(raw_output):
     '''Parses temperature from raw output text.
     '''
-    equals_pos = raw_output.find('t=')
+    equals_pos = raw_output[1].find('t=') # Since raw output is a list, 1st item signals success, 2nd item contains data
     if equals_pos != -1:
         temp_string = raw_output[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
@@ -30,15 +30,15 @@ def parse_temp(raw_output):
 
 def read_temp():
     '''Loops until a successful response is obtained, then parses the raw output and returns the temperature.
-            
-    The raw output will either have YES or NO at the end of the first line. 
+
+    The raw output will either have YES or NO at the end of the first line.
     If it is YES, then the temperature will be at the end of the second line, in degC*1000.
-    
-    Successful response: 
+
+    Successful response:
       4b 01 4b 46 7f ff 05 10 e1 : crc=e1 YES
       4b 01 4b 46 7f ff 05 10 e1 t=31225
-    
-    Failed response: 
+
+    Failed response:
       ... NO  ???
     '''
     raw_output = read_temp_raw()
@@ -50,7 +50,7 @@ def read_temp():
 
     temp_c = parse_temp(raw_output[1])
     return temp_c
-    
+
 if __name__ == '__main__':
     while True:
         print(read_temp())
