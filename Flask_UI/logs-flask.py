4d7c3db73f42b15aaa13a1097669bc045cb3c5cc
WG <wynand@gouws.com.au>
2018-07-09 17:32:48 +0800

    Low ports need sudo

--- Flask_UI/logs-flask.py
+++ Flask_UI/logs-flask.py
@@ -16,4 +16,4 @@ def todays_logs():
         return (todays_temperatures)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
