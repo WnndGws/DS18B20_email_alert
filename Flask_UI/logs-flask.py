35da350b7f3c1124e7d6226f2ed9740884b53189
WG <wynand@gouws.com.au>
2018-07-09 16:32:19 +0800

    Return needs to be in the loop

--- Flask_UI/logs-flask.py
+++ Flask_UI/logs-flask.py
@@ -11,9 +11,9 @@ home_dir = os.path.expanduser("~")
 # Run the following code whenever someone goes to the URL in app route (in this case it is /)
 @app.route("/")
 def todays_logs():
-    with open(f'{home_dir}/RPI-Temp-Sensor/DS18B_email_alert/Logging/logs/freezer01_temperature.log', "r") as f:
+    with open(f'{home_dir}/RPI-Temp-Sensor/DS18B_email_alert/Logging/logs/freezer01_temperature.log', 'r') as f:
         todays_temperatures = f.read()
-    return todays_temperatures
+        return (todays_temperatures)
 
 if __name__ == "__main__":
     app.run(host=0.0.0.0, port=80, debug=True)
