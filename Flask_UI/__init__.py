ea55d12f80e338c41a33af774ff0a7470402f7ee
WG <wynand@gouws.com.au>
2018-07-09 18:16:25 +0800

    rename to match convention

---
+++ Flask_UI/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+##A simple webserver to view raw temperature logs for the day
+
+from flask import Flask
+import os
+
+# Create a flask object called app
+app = Flask(__name__)
+home_dir = os.path.expanduser("~")
+
+# Run the following code whenever someone goes to the URL in app route (in this case it is /)
+@app.route("/")
+def todays_logs():
+    with open('{0}/RPI-Temp-sensor/DS18B20_email_alert/Logging/logs/freezer01_temperature.log'.format(home_dir), 'r') as f:
+        todays_temperatures = f.read()
+        return (todays_temperatures)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
