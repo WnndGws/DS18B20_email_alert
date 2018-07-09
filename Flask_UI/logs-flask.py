c472d974293d982b3eba134a6ba9b33fbdd55e16
WG <wynand@gouws.com.au>
2018-07-09 16:24:03 +0800

    Remove indent

--- Flask_UI/logs-flask.py
+++ Flask_UI/logs-flask.py
@@ -10,10 +10,10 @@ home_dir = os.path.expanduser("~")
 
 # Run the following code whenever someone goes to the URL in app route (in this case it is /)
 @app.route("/")
-    def todays_logs():
-        with open(f'{home_dir}/RPI-Temp-Sensor/DS18B_email_alert/Logging/logs/freezer01_temperature.log') as f:
-            todays_temperatures = f.read()
-        return todays_temperatures
+def todays_logs():
+    with open(f'{home_dir}/RPI-Temp-Sensor/DS18B_email_alert/Logging/logs/freezer01_temperature.log') as f:
+        todays_temperatures = f.read()
+    return todays_temperatures
 
 if __name__ == "__main__":
     app.run(host=0.0.0.0, port=80, debug=True)
