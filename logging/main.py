21f2c24662c300cc194f975cab62f43e39451327
WG <wynand@gouws.com.au>
2018-07-10 10:56:26 +0800

    Renamed Folders

---
+++ logging/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+## Brings everything together
+
+import log_temperature
+import verify_email_address
+import send_emails_with_ini
+import send_sms_with_ini
+
+import time
+
+def keep_latest_2_temps():
+    '''Keeps the last 2 temperatures, to prevent false positive emails. Will only send email when both are above a threshold
+    '''
+    if len(latest_2_temps) == 2:
+        latest_2_temps.pop(0)
+        latest_2_temps.append(log_temperature.read_temp())
+    else:
+        latest_2_temps.append(log_temperature.read_temp())
+
+    return(latest_2_temps)
+
+if __name__ == '__main__':
+    latest_2_temps = [-100, -100]
+    while True:
+        log_temperature.log_temps()
+        keep_latest_2_temps()
+        if (latest_2_temps[0] and latest_2_temps[1]) > 20:
+            send_emails_with_ini.send_email()
+            #send_sms_with_ini.send_sms()
+            seconds_slept = 0
+            while seconds_slept < 43200: # keep logging but dont email
+                time.sleep(2700)
+                seconds_slept += 2700
+                log_temperature.log_temps()
+        time.sleep(2700)
