485bc679185100a54ddfa5a3f127854613e24fc3
WnndGws <WnndGws>
2018-08-16 16:48:08 +0800

    Add scheduler instead of using sleep

--- logging/main.py
+++ logging/main.py
@@ -6,6 +6,7 @@ import verify_email_address
 import send_emails_with_ini
 import send_sms_with_ini
 
+from apscheduler.schedulers.blocking import BlockingScheduler
 import time
 
 def keep_latest_2_temps():
@@ -19,17 +20,17 @@ def keep_latest_2_temps():
 
     return(latest_2_temps)
 
+
 if __name__ == '__main__':
     latest_2_temps = [-100, -100]
-    while True:
-        log_temperature.log_temps()
-        keep_latest_2_temps()
-        if (latest_2_temps[0] and latest_2_temps[1]) > 20:
-            send_emails_with_ini.send_email()
-            #send_sms_with_ini.send_sms()
-            seconds_slept = 0
-            while seconds_slept < 43200: # keep logging but dont email
-                time.sleep(2700)
-                seconds_slept += 2700
-                log_temperature.log_temps()
-        time.sleep(2700)
+    scheduler = BlockingScheduler()
+    scheduler.add_job(log_temperature.log_temps, 'cron', minute='*/5')
+    scheduler.add_job(keep_latest_2_temps, 'cron', minute='*/5')
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
+    if (latest_2_temps[0] and latest_2_temps[1]) > 20:
+        #send_emails_with_ini.send_email()
+        #send_sms_with_ini.send_sms()
+        time.sleep(21600) # keep logging but dont email
